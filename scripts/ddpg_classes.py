7a0934d8e49994aadf6dd566a9290497cea18ebe
jstestsuite <45903619+jstestsuite@users.noreply.github.com>
2019-07-29 16:07:42 -0400

    Add files via upload

--- scripts/ddpg_classes.py
+++ scripts/ddpg_classes.py
@@ -1,6 +1,8 @@
+
 import rospy
 import os
 import json
+
 import random
 import time
 import sys
@@ -9,24 +11,21 @@ import math
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 from collections import deque
 from std_msgs.msg import Float32MultiArray
-#from src.turtlebot3_ddpg.environment_stage_1 import Env
+from src.turtlebot3_ddpg.environment_stage_1 import Env
 import tensorflow as tf
 from tensorflow.keras.models import Sequential, load_model
 from tensorflow.keras.optimizers import RMSprop
 from tensorflow.keras.layers import Dense, Dropout, Activation, Input
+
 from tensorflow import keras
+
 from tensorflow.keras.models import Model
+
+
 from tensorflow.keras import backend
+
 from actor_class import Actor
 from critic_class import Critic
-import ddpg_cenv.cenv as cpp
-
-
-#allows starting a roscpp node from python
-from moveit_ros_planning_interface._moveit_roscpp_initializer import roscpp_init
-
-roscpp_init('node_name', [])
-
 
 EPISODES = 500
 GAMMA = 0.99
@@ -35,11 +34,10 @@ LRA = 0.0001    #Learning rate for Actor
 LRC = 0.001 #Lerning rate for Critic
 state_dim = 14
 batch_size = 32
-MAX_STEPS = 300
+max_step = 401
 memory = deque(maxlen=1000000)
-dirPath = os.path.dirname(os.path.realpath(__file__))
-dirPath = dirPath.replace('turtlebot3_ddpg/nodes', 'turtlebot3_ddpg/save_model/ddpg_')
-load_weight = False
+
+load_weight = True
 
 class OrnsteinUhlenbeckActionNoise:
     def __init__(self, mu, sigma=0.3, theta=.15, dt=1e-2, x0=None):
@@ -66,18 +64,18 @@ class OrnsteinUhlenbeckActionNoise:
     
 
 
-def appendMemory(memory, state, action, reward, next_state):
-       return memory.append((state, action, reward, next_state))
+def appendMemory(memory, state, action, reward, next_state, done):
+       return memory.append((state, action, reward, next_state, done))
 
 #need to analyze
 if __name__ == '__main__':
-    epsilon = 0.04
+    epsilon = 1.0
     epsilon_decay = 0.99
     epsilon_min = 0.04
     rospy.init_node('ddpg_classes')
     pub_result = rospy.Publisher('result', Float32MultiArray, queue_size=5)
     pub_get_action = rospy.Publisher('get_action', Float32MultiArray, queue_size=5)
-    train_indicator = True
+    train_indicator = False
 
     
     sess = tf.Session()
@@ -102,7 +100,7 @@ if __name__ == '__main__':
         except:
 	    print("Cannot find the weight")
 
-    env = cpp.World()
+    env = Env(2)
     #env.unpause_proxy()
 
 
@@ -116,79 +114,69 @@ if __name__ == '__main__':
 
     for e in range(EPISODES):
         done = False
-        env.reset()
-	state = np.asarray(env.step([0.0, 0.0] , [0.0,0.0]))
-	#print state
-	state = state[0:14]
-	#reward = 
+        state = env.reset()
         score = 0
 	
-        for t in range(MAX_STEPS):
+        for t in range(max_step):
 
 	    loss = 0
-	    #print t
-            action = actor.model.predict(state.reshape(1, len(state))) + ( actor_noise()*epsilon)
+
+            action = actor.model.predict(state.reshape(1, len(state))) + ( actor_noise()*epsilon/4)
 	    
 	    
 	    #print "ACTION IS PRINTED"
-            next_state= env.step((action.flatten()).tolist(), (past_action.flatten()).tolist())
-	    reward = next_state[14]
-	    #done = next_state[15]
-	    next_state = np.asarray(next_state[0:14])
+            next_state, reward, done = env.step(action.flatten(), past_action.flatten())
 	    #print len(state), "\n"
-	    #if (train_indicator): 
-          
-	    appendMemory(memory, state, action, reward, next_state)
+	    if (train_indicator):            
+		appendMemory(memory, state, action, reward, next_state, done)
 	    #### Start the training
 
-	    #if len(memory) > batch_size:
-	    if len(memory) > 32:
-            	env.pause_proxy()
-            	mini_batch = random.sample(memory, batch_size)
-		states = np.empty(([0,14]), dtype=np.float64)
-		actions = np.empty(([0,2]), dtype=np.float64)
-		rewards = np.empty(([0,1]), dtype=np.float64)
-		next_states = np.empty(([0,14]), dtype=np.float64)
-		#//dones = []
-		for i in range(batch_size):
-            		states = np.append(states, np.asarray(mini_batch[i][0]).reshape(1, states.shape[1]), axis = 0)
-            		actions = np.append(actions, np.asarray(mini_batch[i][1]).reshape(1, actions.shape[1]), axis = 0)
-            		rewards = np.append(rewards,np.asarray(mini_batch[i][2]).reshape(1, rewards.shape[1]), axis = 0)
-            		next_states = np.append(next_states, np.asarray(mini_batch[i][3]).reshape(1, next_states.shape[1]), axis = 0)
-            		#//dones.append(mini_batch[i][4])
+	    	if len(memory) > batch_size:
+            		env.pause_proxy()
+            		mini_batch = random.sample(memory, batch_size)
+			states = np.empty(([0,14]), dtype=np.float64)
+			actions = np.empty(([0,2]), dtype=np.float64)
+			rewards = np.empty(([0,1]), dtype=np.float64)
+			next_states = np.empty(([0,14]), dtype=np.float64)
+			dones = []
+			for i in range(batch_size):
+            			states = np.append(states, np.asarray(mini_batch[i][0]).reshape(1, states.shape[1]), axis = 0)
+            			actions = np.append(actions, np.asarray(mini_batch[i][1]).reshape(1, actions.shape[1]), axis = 0)
+            			rewards = np.append(rewards,np.asarray(mini_batch[i][2]).reshape(1, rewards.shape[1]), axis = 0)
+            			next_states = np.append(next_states, np.asarray(mini_batch[i][3]).reshape(1, next_states.shape[1]), axis = 0)
+            			dones.append(mini_batch[i][4])
 
 #reshape?
-	    	target_q = critic.target_model.predict([next_states, actor.target_model.predict(next_states)])
+	    		target_q = critic.target_model.predict([next_states, actor.target_model.predict(next_states)])
 
-            	y_i = []
-            	for k in range(batch_size):
+            	    	y_i = []
+            	    	for k in range(batch_size):
 		    
-            	    #if dones[k]:
-            	        #y_i.append(rewards[k])
-            	    #else:
-			y_i.append(rewards[k] + GAMMA * target_q[k])
+            	        #if dones[k]:
+            	          #  y_i.append(rewards[k])
+            	        #else:
+			    y_i.append(rewards[k] + GAMMA * target_q[k])
 
-		h = np.asarray(y_i)
-		#print actions.shape
-		#print states.shape
-		#print h.shape
-                loss += critic.model.train_on_batch([states,actions], h) 
-                a_for_grad = actor.model.predict(states)
-                grads = critic.gradients(states, a_for_grad)
-                actor.train(states, grads)
-                actor.target_train()
-		critic.target_train()
-		env.unpause_proxy()
+			h = np.asarray(y_i)
+			#print actions.shape
+			#print states.shape
+			#print h.shape
+                	loss += critic.model.train_on_batch([states,actions], h) 
+                	a_for_grad = actor.model.predict(states)
+                	grads = critic.gradients(states, a_for_grad)
+                	actor.train(states, grads)
+                	actor.target_train()
+			critic.target_train()
+			env.unpause_proxy()
             	score += reward
             	state = next_state
+	    	if np.mod(t, 100) ==0:
+	    		print "Episode", e, "Step", global_step, "Action", action, "Reward", reward, "Loss", loss
+            if t >= 400:
+                rospy.loginfo("Time out!!")
+                done = True
 
-	    if np.mod(t, 100) ==0:
-	    	print "Episode", e, "Step", global_step, "Action", action, "Reward", reward, "Loss", loss
-            #if t >= 400:
-                #rospy.loginfo("Time out!!")
-                #done = True
-
-            if done or t == MAX_STEPS-1:
+            if done:
                 result.data = [score, loss]
                 pub_result.publish(result)
 		print "publishing reulsts"
@@ -204,15 +192,15 @@ if __name__ == '__main__':
 
             global_step += 1
 
-        if np.mod(e, 20) == 0:
+        if np.mod(e, 10) == 0:
             if (train_indicator):
                 print "Now we save model"
-                actor.model.save_weights(dirPath + str(e) +"_actormodel.h5", overwrite=True)
-                with open(dirPath + str(e) +"_actormodel.json", "w") as outfile:
+                actor.model.save_weights("actormodel.h5", overwrite=True)
+                with open("actormodel.json", "w") as outfile:
                     json.dump(actor.model.to_json(), outfile)
 
-                critic.model.save_weights(dirPath + str(e) +"_criticmodel.h5", overwrite=True)
-                with open(dirPath + str(e) +"_criticmodel.json", "w") as outfile:
+                critic.model.save_weights("criticmodel.h5", overwrite=True)
+                with open("criticmodel.json", "w") as outfile:
                     json.dump(critic.model.to_json(), outfile)
 
         print "TOTAL REWARD @ " + str(e) +"-th Episode  : Reward " + str(score)
