9c52cfc9e0e0dde38c47075177a79693c721be36
jstestsuite <45903619+jstestsuite@users.noreply.github.com>
2019-07-29 16:06:53 -0400

    Add files via upload

---
+++ scripts/environment_stage_1.py
@@ -0,0 +1,181 @@
+#!/usr/bin/env python
+#################################################################################
+# Copyright 2018 ROBOTIS CO., LTD.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#################################################################################
+
+# Authors: Gilbert #
+
+import rospy
+import numpy as np
+import math
+from math import pi
+from geometry_msgs.msg import Twist, Point, Pose
+from sensor_msgs.msg import LaserScan
+from nav_msgs.msg import Odometry
+from std_srvs.srv import Empty
+from tf.transformations import euler_from_quaternion, quaternion_from_euler
+from respawnGoal import Respawn
+
+class Env():
+    def __init__(self, action_size):
+        self.goal_x = 0
+        self.goal_y = 0
+        self.heading = 0
+
+        self.action_size = action_size
+        self.initGoal = True
+        self.get_goalbox = False
+        self.position = Pose()
+        self.pub_cmd_vel = rospy.Publisher('cmd_vel', Twist, queue_size=5)
+        self.sub_odom = rospy.Subscriber('odom', Odometry, self.getOdometry)
+        self.reset_proxy = rospy.ServiceProxy('gazebo/reset_simulation', Empty)
+        self.unpause_proxy = rospy.ServiceProxy('gazebo/unpause_physics', Empty)
+        self.pause_proxy = rospy.ServiceProxy('gazebo/pause_physics', Empty)
+        self.respawn_goal = Respawn()
+        self.past_distance = 0.0
+
+ 
+    def unpause_proxy(self):
+        self.unpause_proxy   
+    def pause_proxy(self):
+        self.pause_proxy
+
+    def getGoalDistace(self):
+        goal_distance = math.hypot(self.goal_x - self.position.x, self.goal_y - self.position.y)
+
+        return goal_distance
+
+    def getOdometry(self, odom):
+        self.position = odom.pose.pose.position
+        orientation = odom.pose.pose.orientation
+        orientation_list = [orientation.x, orientation.y, orientation.z, orientation.w]
+        _, _, yaw = euler_from_quaternion(orientation_list)
+
+        goal_angle = math.atan2(self.goal_y - self.position.y, self.goal_x - self.position.x)
+
+        heading = goal_angle - yaw
+        if heading > pi:
+            heading -= 2 * pi
+
+        elif heading < -pi:
+            heading += 2 * pi
+
+        self.heading = round(heading, 2)
+
+    def getState(self, scan, past_action):
+        scan_range = []
+        heading = self.heading
+        min_range = 0.15
+        done = False
+
+        for i in range(len(scan.ranges)):
+            if scan.ranges[i] == float('Inf'):
+                scan_range.append(3.5)
+            elif np.isnan(scan.ranges[i]):
+                scan_range.append(0)
+            else:
+                scan_range.append(scan.ranges[i])
+
+        if min_range > min(scan_range) > 0:
+            done = True
+
+        current_distance = round(math.hypot(self.goal_x - self.position.x, self.goal_y - self.position.y),2)
+        if current_distance < 0.2:
+            self.get_goalbox = True
+
+        return scan_range + [past_action[0], past_action[1], heading, current_distance], done
+
+    def setReward(self, state, done, action):
+
+        current_distance = state[-1]
+        heading = state[-2]
+
+
+        distance_rate = (self.past_distance - current_distance) 
+
+
+        reward = (500 * distance_rate)
+        self.past_distance = current_distance
+        
+	if done:
+            rospy.loginfo("Collision!!")
+            reward = -150
+            self.pub_cmd_vel.publish(Twist())
+
+        if self.get_goalbox:
+            rospy.loginfo("Goal!!")
+            reward = 200
+            self.pub_cmd_vel.publish(Twist())
+            self.goal_x, self.goal_y = self.respawn_goal.getPosition(True, delete=True)
+            self.goal_distance = self.getGoalDistace()
+            self.get_goalbox = False
+
+        return reward
+
+    def step(self, action, past_action):
+	#print "Action: ", action
+        ang_vel = action[1]
+	if ang_vel >= 1.0:
+		ang_vel = 1.0
+	if ang_vel <= -1.0:
+		ang_vel = -1.0
+        vel_cmd = Twist()
+        vel_cmd.linear.x = action[0]*0.3
+        vel_cmd.angular.z = ang_vel
+        self.pub_cmd_vel.publish(vel_cmd)
+
+        
+
+        data = None
+        while data is None:
+            try:
+                data = rospy.wait_for_message('scan', LaserScan, timeout=5)
+            except:
+                pass
+
+        state, done = self.getState(data, past_action)
+        reward = self.setReward(state, done, action)
+
+        return np.asarray(state), reward, done
+
+    def reset(self):
+        rospy.wait_for_service('gazebo/reset_simulation')
+        try:
+            self.reset_proxy()
+        except (rospy.ServiceException) as e:
+            print("gazebo/reset_simulation service call failed")
+
+        data = None
+        while data is None:
+            try:
+                data = rospy.wait_for_message('scan', LaserScan, timeout=5)
+            except:
+                pass
+
+        if self.initGoal:
+            self.goal_x, self.goal_y = self.respawn_goal.getPosition()
+            self.initGoal = False
+
+        self.goal_distance = self.getGoalDistace()
+        state, done = self.getState(data, [0.,0.])
+
+        return np.asarray(state)
+
+
+
+
+
+
+
